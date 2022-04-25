824984f21f1909348b6a54de8afc03f425d4dc19
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-25 18:41:07 +0200

    refactor: extract machine to separate file to break class bonds

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Config } from "./Config";
 import { waitFor } from 'xstate/lib/waitFor'
 import { actions, ActorRef, AnyInterpreter, AnyState, AnyStateMachine, assign, createMachine, DoneInvokeEvent, EventObject, interpret, send, spawn } from "xstate";
-
+import { machine } from "./machine";
 type InitEvent = { type: 'INIT' }
 type SaveRowEvent = { type: 'SAVE_ROW', id: string }
 type DeleteRowEvent = { type: 'DELETE_ROW', id: string }
@@ -33,144 +33,21 @@ export class XStateDB {
     }
 
     private createMachine() {
-        const context = {
-            rowsRefs: {} as Record<string, ActorRef<any>>,
-            tableRef: null as ActorRef<any> | null,
-            saverRefs: [] as ActorRef<any>[],
-            unfinishedRowsIDs: [] as string[]
-        }
-        type Context = typeof context
-        const machine = createMachine<Context, Event>({
-            initial: 'loadingTableMachine',
-            context,
-            states: {
-                loadingTableMachine: {
-                    invoke: {
-                        src: 'loadTableMachine',
-                        onDone: { target: 'loadingRowsMachines', actions: 'spawnTableMachine' },
-                        onError: 'failed'
-                    }
-                },
-                loadingRowsMachines: {
-                    invoke: {
-                        src: 'loadRowsMachines',
-                        onDone: { target: 'startingTableMachine', actions: 'spawnRowsMachines' },
-                        onError: 'failed'
-                    }
-                },
-                startingTableMachine: {
-                    entry: send<Context, Event>('INIT', { to: (ctx) => ctx.tableRef! }),
-                    on: { always: 'executingRows' }
-                },
-                executingRows: {
-                    entry: 'sendRowsToTable',
-                    on: {
-                        SAVE_ROW: { actions: 'saveRow' },
-                        DELETE_ROW: { actions: ['deleteRow', 'markRowFinished'] },
-                        ROW_FINISHED: [
-                            { actions: ['markRowFinished'] },
-                            { cond: 'lastRowFinished', actions: 'sendAllRowsFinishedToTable' },
-                        ],
-                        CREATE_ROW: { actions: ['createRow', 'markRowUnfinished', 'sendCreatedRowToTable'] },
-                        STOP: 'done',
-                    },
-                    exit: ['stopTableMachine', 'stopRowsMachines']
-                },
-                failed: {
-                    type: 'final'
-                },
-                done: {
-                    type: 'final'
-                }
-            }
-        }, {
-            guards: {
-                lastRowFinished: (ctx, evt) =>
-                    ctx.unfinishedRowsIDs.filter(id => id != (evt as RowFinishedEvent).id)
-                        .length == 0
-            },
-            actions: {
-                spawnTableMachine: assign<Context, DoneInvokeEvent<AnyStateMachine>>({
-                    tableRef: (_, evt) => spawn(evt.data, { sync: true }),
-                }),
-                spawnRowsMachines: assign<Context, DoneInvokeEvent<AnyStateMachine[]>>({
-                    rowsRefs: (ctx, evt) =>
-                        Object.assign({},
-                            ctx.rowsRefs,
-                            ...evt.data.map(machine => ({ [machine.id]: spawn(machine, { sync: true }) }))
-                        ),
-                    unfinishedRowsIDs: (ctx, evt) => [
-                        ...ctx.unfinishedRowsIDs,
-                        ...evt.data.map(machine => machine.id)
-                    ]
-                }),
-                sendRowsToTable: actions.pure<Context, Event>((ctx) => {
-                    return Object.keys(ctx.rowsRefs).map(rowID => ctx.rowsRefs[rowID])
-                        .map(rowRef =>
-                            send<Context, RowInitializedEvent>(
-                                { type: 'ROW_INITIALIZED', rowState: rowRef.getSnapshot() },
-                                { to: (ctx) => ctx.tableRef! }
-                            ));
-                }),
-                saveRow: assign<Context, SaveRowEvent>({
-                    saverRefs: (ctx, evt) => [
-                        ...ctx.saverRefs,
-                        spawn(this.config.storage.store(evt.id, ctx.rowsRefs[evt.id].getSnapshot()))
-                    ]
-                }),
-                createRow: assign<Context, CreateRowEvent>({
-                    rowsRefs: (ctx, evt) =>
-                        Object.assign({},
-                            ctx.rowsRefs,
-                            { [evt.machine.id]: spawn(evt.machine, { sync: true }) }
-                        ),
-                }),
-                sendCreatedRowToTable: send<Context, CreateRowEvent | RowInitializedEvent>(
-                    (ctx, evt) => ({ type: 'ROW_INITIALIZED', rowState: ctx.rowsRefs[(evt as CreateRowEvent).machine.id].getSnapshot() }),
-                    { to: (ctx) => ctx.tableRef! }
-                ),
-                deleteRow: actions.pure<Context, DeleteRowEvent>((_, evt) => {
-                    return [
-                        actions.stop<Context, Event>((ctx) => ctx.rowsRefs[evt.id]),
-                        assign<Context, DeleteRowEvent>({
-                            rowsRefs: (ctx, evt) =>
-                                Object.assign({},
-                                    ...Object.keys(ctx.rowsRefs)
-                                        .filter(id => id != evt.id)
-                                        .map(id => ({ [id]: ctx.rowsRefs[id] })),
-                                ),
-                        }),
-                    ]
-                }),
-                markRowUnfinished: assign<Context, CreateRowEvent>({
-                    unfinishedRowsIDs: (ctx, evt) =>
-                        [
-                            ...ctx.unfinishedRowsIDs,
-                            evt.machine.id,
-                        ]
-                }),
-                markRowFinished: assign<Context, RowFinishedEvent | DeleteRowEvent>({
-                    unfinishedRowsIDs: (ctx, evt) =>
-                        ctx.unfinishedRowsIDs.filter(id => id != evt.id)
-                }),
-                sendAllRowsFinishedToTable: send<Context, AllRowsFinishedEvent>(
-                    { type: 'ALL_ROWS_FINISHED' },
-                    { to: (ctx) => ctx.tableRef! }
-                ),
-                stopTableMachine: actions.stop<Context, Event>((ctx) => ctx.tableRef!),
-                stopRowsMachines: actions.pure<Context, Event>((ctx) => {
-                    return Object.keys(ctx.rowsRefs).map(rowID => ctx.rowsRefs[rowID])
-                        .map(rowRef => actions.stop<Context, Event>(rowRef.id));
-                }),
-            }
-        });
         return machine
+            .withContext({
+                ...machine.context,
+                saver: (id: string, state: AnyState) => this.config.storage.store(id, state)
+            })
+            .withConfig({
+                services: {
+                    loadTableMachine,
+                    loadRowsMachines
+                }
+            })
     }
 
     private interpretMachine(machine: ReturnType<typeof this.createMachine>) {
-        return interpret(machine.withConfig({
-            services: {}
-        }))
+        return interpret(machine)
     }
 
     private async waitForInterpreter(interpreter: AnyInterpreter): Promise<void> {
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,6 @@
 export interface XStateDBStorage {
     store(id: string, state: object): Promise<void>
+    exists(id: string): Promise<boolean>
     read(id: string): Promise<object>
     delete(id: string): Promise<void>
     getAllIDs(): Promise<string[]>
